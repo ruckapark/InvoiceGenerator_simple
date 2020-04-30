552ffe4c7f2945aecd08b86af74c60225eb9a723
George Ruck <georgeadruck@gmail.com>
2020-04-30 17:03:10 +0200

    Using InvoiceGenerator

---
+++ generate_inv.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 16:36:46 2020
+
+Simple template for creating invoices.
+
+@author: George
+"""
+
+#read in text file with my standard info
+
+
+#add in specific parameters (date should be automatic etc.)
+
+
+#update text file with next invoice number
